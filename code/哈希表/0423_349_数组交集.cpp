2f0b0c607595cff7a88e33d9cffbdda4d296ef7c
ZhengzhiVon <1455612471@qq.com>
2023-04-23 20:38:03 +0800

    349补充数组作hash

--- code/哈希表/0423_349_数组交集.cpp
+++ code/哈希表/0423_349_数组交集.cpp
@@ -14,3 +14,26 @@ public:
         return vector<int>(result_set.begin(), result_set.end());
     }
 };
+
+/*
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+所以就可以 使用数组来做哈希表了， 因为数组都是1000以内的。
+*/
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> result_set; // 存放结果，之所以用set是为了给结果集去重
+        int hash[1005] = {0}; // 默认数值为0
+        for (int num : nums1) { // nums1中出现的字母在hash数组中做记录
+            hash[num] = 1;
+        }
+        for (int num : nums2) { // nums2中出现话，result记录
+            if (hash[num] == 1) {
+                result_set.insert(num);
+            }
+        }
+        return vector<int>(result_set.begin(), result_set.end());
+    }
+};
