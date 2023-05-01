220a109aa1ffc75ae7dc8c96fda5b6c2b779e8ea
ZhengzhiVon <1455612471@qq.com>
2023-05-01 21:38:24 +0800

    541_反转字符串2

---
+++ code/字符串/541_反转字符串2.cpp
@@ -0,0 +1,54 @@
+//用库函数reverse()
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            // 1. 每隔 2k 个字符的前 k 个字符进行反转
+            // 2. 剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符
+            if (i + k <= s.size()) {
+                reverse(s.begin() + i, s.begin() + i + k );
+            } else {
+                // 3. 剩余字符少于 k 个，则将剩余字符全部反转。
+                reverse(s.begin() + i, s.end());
+            }
+        }
+        return s;
+    }
+};
+
+class Solution {
+public:
+    void reverse(string& s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            swap(s[i], s[j]);
+        }
+    }
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            // 1. 每隔 2k 个字符的前 k 个字符进行反转
+            // 2. 剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符
+            if (i + k <= s.size()) {
+                reverse(s, i, i + k - 1);
+                continue;
+            }
+            // 3. 剩余字符少于 k 个，则将剩余字符全部反转。
+            reverse(s, i, s.size() - 1);
+        }
+        return s;
+    }
+};
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size(),pos = 0;
+        while(pos < n){
+            //剩余字符串大于等于k的情况
+            if(pos + k < n) reverse(s.begin() + pos, s.begin() + pos + k);
+            //剩余字符串不足k的情况 
+            else reverse(s.begin() + pos,s.end());
+            pos += 2 * k;
+        }
+        return s;
+    }
+};
